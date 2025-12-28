0f2e41d72fc0532e71659ef8e7a1d8e1d7149905
Sinaps Dev <rayan.sinaps@gmail.com>
2025-12-28 17:10:26 +0100

    IsVehicleOwnedByPlayer method

--- modules/framework/qb-core/server.lua
+++ modules/framework/qb-core/server.lua
@@ -633,6 +633,20 @@ Framework.GetOwnedVehicles = function(src)
     return vehicles
 end
 
+---@description Returns a table of owned vehicles for the player. format is {id = id, vehicle = model, plate = plate}
+---@param src number
+---@param plate string
+---@return table
+Framework.IsVehicleOwnedByPlayer = function(src, plate)
+    local citizenId = Framework.GetPlayerIdentifier(src)
+    local result = MySQL.Sync.fetchAll("SELECT id, vehicle, plate FROM player_vehicles WHERE citizenid = '" .. citizenId .. "' AND plate = '" .. plate .. "'")
+    if not result[1] then return false end
+
+    local id = result[1].id
+    local vehicle = result[1].vehicle
+    return { id = id, vehicle = vehicle, plate = plate }
+end
+
 ---@description Registers a usable item with a callback function.
 ---@param itemName string
 ---@param cb function
